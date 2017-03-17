2572bd9dd9186ee36a3bea7ce4e9c04c9d25ed06
root <sbappan@ncsu.edu>
2017-03-17 12:39:07 -0400

    Added ovs_private and ovs_public networks

--- install_ovs.sh
+++ install_ovs.sh
@@ -28,13 +28,13 @@ ovs-vsctl show
 
 # Add ovs private and public bridges
 ovs-vsctl add-br ovs_br0
-ovs-vsctl add-br ovs_br1
+#ovs-vsctl add-br ovs_br1
 
 # Define ovs private and public networks
 virsh net-define ./ovs_private.xml
 virsh net-start ovs_private
 virsh net-autostart ovs_private
 
-virsh net-define ./ovs_public.xml
-virsh net-start ovs_public
-virsh net-autostart ovs_public
+#virsh net-define ./ovs_public.xml
+#virsh net-start ovs_public
+#virsh net-autostart ovs_public
