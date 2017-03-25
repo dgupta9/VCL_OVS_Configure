0adfeb8b9c12a294f08c2e2942c2daa269c5c1cb
root <root@bn17-10.dcs.mcnc.org>
2017-03-25 17:38:24 -0400

    Restart sshd and iptables

--- set_ovs_nw.sh
+++ set_ovs_nw.sh
@@ -19,6 +19,7 @@ ssh mn << EOF
   chmod 755 ./mn_nw.sh
   ./mn_nw.sh
 EOF
+# Need to add code to make eth0 IP as static
 
 sed  '/192.168.100.1 mn/c\192.168.200.1 mn' /etc/hosts > tmp && mv -f tmp /etc/hosts
 virsh dumpxml managementnode > /tmp/mn.xml
@@ -26,9 +27,16 @@ virsh shutdown managementnode
 sed "s/\bprivate\b/ovs_private/g" /tmp/mn.xml > tmp && mv -f tmp /tmp/mn.xml
 sed "s/\bvirbr0\b/ovs_br0/g" /tmp/mn.xml > tmp && mv -f tmp /tmp/mn.xml
 virsh undefine managementnode
+sleep 5 # Need to change this
 virsh create /tmp/mn.xml
 
 # Handle dnsmasq. See config files in /var/lib/libvirt/dnsmasq/
 ps -ef | grep "dnsmasq/private" | grep -v grep | awk '{print $2}' | xargs kill
 ifconfig virbr0 0 down
 ifconfig ovs_br0 192.168.100.10 up
+systemctl restart hostonly_sshd
+systemctl status hostonly_sshd
+sed "s/\bvirbr0\b/ovs_br0/g" /etc/sysconfig/iptables > tmp && mv -f tmp /etc/sysconfig/iptables
+systemctl restart iptables
+systemctl status iptables
+
