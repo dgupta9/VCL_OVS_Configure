cc66e9a00aa4c045f0115c376c31910915a7c5af
root <root@bn20-60.dcs.mcnc.org>
2017-03-25 00:47:01 -0400

    Change mn to be connected to ovs_private

--- mn_nw.sh
+++ mn_nw.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-sed '/ListenAddress 192.168.100.1/a ListenAddress 192.168.200.1' /etc/ssh/sshd_config > tmp && mv -f tmp /etc/ssh/sshd_config
-
+#sed '/ListenAddress 192.168.100.1/a ListenAddress 192.168.200.1' /etc/ssh/sshd_config > tmp && mv -f tmp /etc/ssh/sshd_config
+echo "ListenAddress 192.168.200.1" >> /etc/ssh/sshd_config
+systemctl restart sshd
+systemctl status sshd
--- set_ovs_nw.sh
+++ set_ovs_nw.sh
@@ -19,3 +19,11 @@ ssh mn << EOF
   chmod 755 ./mn_nw.sh
   ./mn_nw.sh
 EOF
+
+sed  '/192.168.100.1 mn/c\192.168.200.1 mn' /etc/hosts > tmp && mv -f tmp /etc/hosts
+virsh dumpxml managementnode > /tmp/mn.xml
+virsh shutdown managementnode
+sed "s/\bprivate\b/ovs_private/g" /tmp/mn.xml > tmp && mv -f tmp /tmp/mn.xml
+sed "s/\bvirbr0\b/ovs_br0/g" /tmp/mn.xml > tmp && mv -f tmp /tmp/mn.xml
+virsh undefine managementnode
+virsh create /tmp/mn.xml
