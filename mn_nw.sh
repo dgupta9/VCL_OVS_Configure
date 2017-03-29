324cb24b04eb37b74201ac98cb00c6c23f5f2d17
root <root@bn16-219.dcs.mcnc.org>
2017-03-29 15:48:17 -0400

    Moved code from ssh from host to mn_nw script

--- mn_nw.sh
+++ mn_nw.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
+sed "s/\bBOOTPROTO=dhcp\b/BOOTPROTO=none/g" /etc/sysconfig/network-scripts/ifcfg-eth0 > tmp && mv -f tmp /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "DEVICE=eth0" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "IPADDR=192.168.100.1" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "NETMASK=255.255.255.0" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "NM_CONTROLLED=no" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "NM_CONTROLLED=yes" >> /etc/sysconfig/network-scripts/ifcfg-eth1
 systemctl restart network
+
 #echo "ListenAddress 192.168.200.1" >> /etc/ssh/sshd_config
 #systemctl restart sshd
 #systemctl status sshd -l
