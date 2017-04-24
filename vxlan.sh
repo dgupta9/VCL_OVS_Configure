3aab84787fa698ba1a55caf0a6c99ff0c85bb8de
root <sbappan@ncsu.edu>
2017-04-23 20:03:25 -0400

    Incorporated VXLAN tunnel in eth0. Still need to test ssh

--- vxlan.sh
+++ vxlan.sh
@@ -8,22 +8,27 @@ if [ "$#" -ne 1 ] || ([ "$1" != "master" ] && [ "$1" != "slave" ]); then
     exit 1
 fi
 
-sandbox_info_file="./sandbox_config"
-my_IP="$(ifconfig eth1 | grep -w "inet" | cut -d 'i' -f 2 | cut -d ' ' -f 2)"
-num_sandboxes="$(cat "$sandbox_info_file" | sed '/^\s*#/d;/^\s*$/d' | wc -l)"
+sandbox_private="./sandbox_private_config"
+my_private_IP="$(ifconfig eth0 | grep -w "inet" | cut -d 'i' -f 2 | cut -d ' ' -f 2)"
+num_sandboxes="$(cat "$sandbox_private" | sed '/^\s*#/d;/^\s*$/d' | wc -l)"
+ips_private=() # Create array to store private IPs of the sandboxes
+ips_public=() # Create array to store public IPs of the sandboxes
 
-echo "My IP is $my_IP"
+echo "My IP is $my_private_IP"
 echo "Number of sandboxes =$num_sandboxes"
 
 get_IP_array() {
-    ips=() # Create array to store IPs of the sandboxes
     while IFS= read -r line # Read a line
     do
         # Check for empty or commented lines
         if [[ $(echo $line | sed '/^\s*#/d;/^\s*$/d') ]]; then
-              ips+=("$line") # Append line to the array
+              if [ "$1" == "private" ]; then
+                   ips_private+=("$line") # Append line to the array
+              else
+                   ips_public+=("$line") # Append line to the array
+              fi
         fi
-    done < "$1"
+    done < "$2"
 }
 
 
@@ -35,15 +40,15 @@ sleep 1
 iptables -I INPUT -p udp -m udp --dport 4789 -j ACCEPT
 service iptables save
 
-get_IP_array "$sandbox_info_file"
+get_IP_array "private" "$sandbox_private"
 
-for ((i=0; i<${#ips[@]}; i++));
+for ((i=0; i<${#ips_private[@]}; i++));
 do
-    ip=`echo "${ips[$i]}" | cut -d "=" -f2` # Cut only IP addresses
-    ips[$i]=${ip// } # Remove trailing characters if any
-    if [ "${ips[$i]}" != "$my_IP" ]; then
-         ovs-vsctl add-port ovs_br0 vx$i -- set interface vx$i type=vxlan options:remote_ip="${ips[$i]}"
-         echo "Tunneling from $my_IP to ${ips[$i]}"
+    ip_private=`echo "${ips_private[$i]}" | cut -d "=" -f2` # Cut only IP addresses
+    ips_private[$i]=${ip_private// } # Remove trailing characters if any
+    if [ "${ips_private[$i]}" != "$my_private_IP" ]; then
+         ovs-vsctl add-port ovs_br0 vx$i -- set interface vx$i type=vxlan options:remote_ip="${ips_private[$i]}"
+         echo "Tunneling from $my_private_IP to ${ips_private[$i]}"
     fi
 done
 
