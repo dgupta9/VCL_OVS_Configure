199fd3ce345b848fd501c74e6a5cd6fec936402e
root <sbappan@ncsu.edu>
2017-05-02 09:21:23 -0400

    Changes to handle scalability without copying config files

--- main.sh
+++ main.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-if [ "$#" -ne 1 ] || ([ "$1" != "master" ] && [ "$1" != "slave" ]); then
-    echo "Illegal usage. Please run it as \"./main.sh <master/slave>\""
+if [ "$#" -gt 2 ] || [ "$#" -lt 1 ] || ([ "$1" != "master" ] && [ "$1" != "slave" ]); then
+    echo "Illegal usage. Please run it as \"./main.sh <master/slave> <sandbox_no>\""
     exit 1
 fi
-./install_ovs.sh
-./set_ovs_nw.sh "$1"
-./vxlan.sh "$1"
-./cleanup.sh
+#./install_ovs.sh
+#./set_ovs_nw.sh "$1" "$2"
+#./vxlan.sh "$1"
+#./cleanup.sh
